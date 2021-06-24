2c0534036cf25176fc93ddb98838f9a2df58f84c
Jenny Viannay <32166518+JennyViannay@users.noreply.github.com>
2021-06-24 20:58:36 +0200

    Initial commit

---
+++ src/Controller/BoatController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Boat;
+use App\Repository\BoatRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/boat")
+ */
+class BoatController extends AbstractController
+{
+    /**
+     * Move the boat to coord x,y
+     * @Route("/move/{x}/{y}", name="moveBoat", requirements={"x"="\d+", "y"="\d+"}))
+     */
+    public function moveBoat(int $x, int $y, BoatRepository $boatRepository, EntityManagerInterface $em): Response
+    {
+        $boat = $boatRepository->findOneBy([]);
+        $boat->setCoordX($x);
+        $boat->setCoordY($y);
+        $em->flush();
+        return $this->redirectToRoute('map');
+    }
+}
